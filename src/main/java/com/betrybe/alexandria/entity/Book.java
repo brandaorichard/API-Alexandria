0fbf68b4fc6a54776b58dc7d71eff857d32ecbca
brandaorichard <rih.brandao@gmail.com>
2024-05-30 02:59:34 -0300

    fix error using '*' as import from jakarta

--- src/main/java/com/betrybe/alexandria/entity/Book.java
+++ src/main/java/com/betrybe/alexandria/entity/Book.java
@@ -1,6 +1,10 @@
 package com.betrybe.alexandria.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 /**
  * The type Book.
