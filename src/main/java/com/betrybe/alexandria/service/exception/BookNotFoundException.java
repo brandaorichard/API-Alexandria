412ce6d6bf0e6b2df872606b159c3582f5e52ebb
brandaorichard <rih.brandao@gmail.com>
2024-05-30 02:54:39 -0300

    feat: add javadocs to exceptions handlers

--- src/main/java/com/betrybe/alexandria/service/exception/BookNotFoundException.java
+++ src/main/java/com/betrybe/alexandria/service/exception/BookNotFoundException.java
@@ -1,7 +1,13 @@
 package com.betrybe.alexandria.service.exception;
 
+/**
+ * The type Book not found exception.
+ */
 public class BookNotFoundException extends NotFoundException {
 
+  /**
+   * Instantiates a new Book not found exception.
+   */
   public BookNotFoundException() {
     super("Livro não encontrado!");
   }
--- src/main/java/com/betrybe/alexandria/service/exception/PublisherNotFoundException.java
+++ src/main/java/com/betrybe/alexandria/service/exception/PublisherNotFoundException.java
@@ -1,7 +1,13 @@
 package com.betrybe.alexandria.service.exception;
 
+/**
+ * The type Publisher not found exception.
+ */
 public class PublisherNotFoundException extends NotFoundException {
 
+  /**
+   * Instantiates a new Publisher not found exception.
+   */
   public PublisherNotFoundException() {
     super("Editora não encontrada!");
   }
