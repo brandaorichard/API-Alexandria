e48fd88b0ab34178415e3760345010abc71ee509
brandaorichard <rih.brandao@gmail.com>
2024-05-30 16:43:02 -0300

    feat: create GlobalControllerAdvice to handle errors in the controller layer

---
+++ src/main/java/com/betrybe/alexandria/advice/GlobalControllerAdvice.java
@@ -0,0 +1,16 @@
+package com.betrybe.alexandria.advice;
+
+import com.betrybe.alexandria.service.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class GlobalControllerAdvice {
+
+  @ExceptionHandler
+  public ResponseEntity<String> handleNotFound(NotFoundException exception) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .body(exception.getMessage());
+  }
+}
