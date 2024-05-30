a3f3897f49be9e37f6e516c1327c471da837d710
brandaorichard <rih.brandao@gmail.com>
2024-05-30 15:57:51 -0300

    feat: add javadocs comments to service layer

--- src/main/java/com/betrybe/alexandria/service/BookService.java
+++ src/main/java/com/betrybe/alexandria/service/BookService.java
@@ -7,29 +7,63 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * The type Book service.
+ */
 @Service
 public class BookService {
 
   private final BookRepository bookRepository;
 
+  /**
+   * Instantiates a new Book service.
+   *
+   * @param bookRepository the book repository
+   */
   @Autowired
   public BookService(BookRepository bookRepository) {
     this.bookRepository = bookRepository;
   }
 
+  /**
+   * Find by id book.
+   *
+   * @param id the id
+   * @return the book
+   * @throws BookNotFoundException the book not found exception
+   */
   public Book findById(Long id) throws BookNotFoundException {
     return bookRepository.findById(id)
         .orElseThrow(BookNotFoundException::new);
   }
 
+  /**
+   * Find all list.
+   *
+   * @return the list
+   */
   public List<Book> findAll() {
     return bookRepository.findAll();
   }
 
+  /**
+   * Create book.
+   *
+   * @param book the book
+   * @return the book
+   */
   public Book create(Book book) {
     return bookRepository.save(book);
   }
 
+  /**
+   * Update book.
+   *
+   * @param id   the id
+   * @param book the book
+   * @return the book
+   * @throws BookNotFoundException the book not found exception
+   */
   public Book update(Long id, Book book) throws BookNotFoundException {
     Book bookFromDb = findById(id);
 
@@ -39,6 +73,13 @@ public class BookService {
     return bookRepository.save(bookFromDb);
   }
 
+  /**
+   * Delete by id book.
+   *
+   * @param id the id
+   * @return the book
+   * @throws BookNotFoundException the book not found exception
+   */
   public Book deleteById(Long id) throws BookNotFoundException {
     Book book = findById(id);
 
--- src/main/java/com/betrybe/alexandria/service/PublisherService.java
+++ src/main/java/com/betrybe/alexandria/service/PublisherService.java
@@ -7,29 +7,63 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * The type Publisher service.
+ */
 @Service
 public class PublisherService {
 
   private final PublisherRepository publisherRepository;
 
+  /**
+   * Instantiates a new Publisher service.
+   *
+   * @param publisherRepository the publisher repository
+   */
   @Autowired
   public PublisherService(PublisherRepository publisherRepository) {
     this.publisherRepository = publisherRepository;
   }
 
+  /**
+   * Find by id publisher.
+   *
+   * @param id the id
+   * @return the publisher
+   * @throws PublisherNotFoundException the publisher not found exception
+   */
   public Publisher findById(Long id) throws PublisherNotFoundException {
     return publisherRepository.findById(id)
         .orElseThrow(PublisherNotFoundException::new);
   }
 
+  /**
+   * Find all list.
+   *
+   * @return the list
+   */
   public List<Publisher> findAll() {
     return publisherRepository.findAll();
   }
 
+  /**
+   * Create publisher.
+   *
+   * @param publisher the publisher
+   * @return the publisher
+   */
   public Publisher create(Publisher publisher) {
     return publisherRepository.save(publisher);
   }
 
+  /**
+   * Update publisher.
+   *
+   * @param id        the id
+   * @param publisher the publisher
+   * @return the publisher
+   * @throws PublisherNotFoundException the publisher not found exception
+   */
   public Publisher update(Long id, Publisher publisher) throws PublisherNotFoundException {
     Publisher publisherFromDb = findById(id);
 
@@ -39,6 +73,13 @@ public class PublisherService {
     return publisherRepository.save(publisherFromDb);
   }
 
+  /**
+   * Delete by id publisher.
+   *
+   * @param id the id
+   * @return the publisher
+   * @throws PublisherNotFoundException the publisher not found exception
+   */
   public Publisher deleteById(Long id) throws PublisherNotFoundException {
     Publisher publisher = findById(id);
 
