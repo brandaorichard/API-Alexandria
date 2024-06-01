fa416dfcc058a38705e8645af7c211efa0a15fa6
brandaorichard <rih.brandao@gmail.com>
2024-06-01 01:43:33 -0300

    feat: additional changes to repos

--- src/main/java/com/betrybe/alexandria/repository/AuthorRepository.java
+++ src/main/java/com/betrybe/alexandria/repository/AuthorRepository.java
@@ -4,9 +4,6 @@ import com.betrybe.alexandria.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * The interface Author repository.
- */
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
