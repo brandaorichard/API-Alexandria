8b9f81b4bd4d08e1b511debad9856ebebec67e70
brandaorichard <rih.brandao@gmail.com>
2024-06-01 00:35:16 -0300

    feat: implements CRUD to bookDetail at service layer

--- src/main/java/com/betrybe/alexandria/service/BookService.java
+++ src/main/java/com/betrybe/alexandria/service/BookService.java
@@ -1,69 +1,40 @@
 package com.betrybe.alexandria.service;
 
 import com.betrybe.alexandria.entity.Book;
+import com.betrybe.alexandria.entity.BookDetail;
+import com.betrybe.alexandria.repository.BookDetailRepository;
 import com.betrybe.alexandria.repository.BookRepository;
+import com.betrybe.alexandria.service.exception.BookDetailNotFoundException;
 import com.betrybe.alexandria.service.exception.BookNotFoundException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/**
- * The type Book service.
- */
 @Service
 public class BookService {
 
   private final BookRepository bookRepository;
+  private final BookDetailRepository bookDetailRepository;
 
-  /**
-   * Instantiates a new Book service.
-   *
-   * @param bookRepository the book repository
-   */
   @Autowired
-  public BookService(BookRepository bookRepository) {
+  public BookService(BookRepository bookRepository, BookDetailRepository bookDetailRepository) {
     this.bookRepository = bookRepository;
+    this.bookDetailRepository = bookDetailRepository;
   }
 
-  /**
-   * Find by id book.
-   *
-   * @param id the id
-   * @return the book
-   * @throws BookNotFoundException the book not found exception
-   */
   public Book findById(Long id) throws BookNotFoundException {
     return bookRepository.findById(id)
         .orElseThrow(BookNotFoundException::new);
   }
 
-  /**
-   * Find all list.
-   *
-   * @return the list
-   */
   public List<Book> findAll() {
     return bookRepository.findAll();
   }
 
-  /**
-   * Create book.
-   *
-   * @param book the book
-   * @return the book
-   */
   public Book create(Book book) {
     return bookRepository.save(book);
   }
 
-  /**
-   * Update book.
-   *
-   * @param id   the id
-   * @param book the book
-   * @return the book
-   * @throws BookNotFoundException the book not found exception
-   */
   public Book update(Long id, Book book) throws BookNotFoundException {
     Book bookFromDb = findById(id);
 
@@ -73,18 +44,62 @@ public class BookService {
     return bookRepository.save(bookFromDb);
   }
 
-  /**
-   * Delete by id book.
-   *
-   * @param id the id
-   * @return the book
-   * @throws BookNotFoundException the book not found exception
-   */
   public Book deleteById(Long id) throws BookNotFoundException {
     Book book = findById(id);
 
     bookRepository.deleteById(id);
 
     return book;
+  }
+
+  // Book Details:
+  public BookDetail createBookDetail(Long bookId, BookDetail bookDetail)
+      throws BookNotFoundException {
+    Book book = findById(bookId);
+    bookDetail.setBook(book);
+
+    return bookDetailRepository.save(bookDetail);
+  }
+
+  public BookDetail getBookDetail(Long bookId)
+      throws BookNotFoundException, BookDetailNotFoundException {
+    Book book = findById(bookId);
+
+    BookDetail bookDetailFromDb = book.getDetails();
+
+    if (bookDetailFromDb == null) {
+      throw new BookDetailNotFoundException();
+    }
+
+    return bookDetailFromDb;
+  }
+
+  public BookDetail updateBookDetail(Long bookId, BookDetail bookDetail)
+      throws BookNotFoundException, BookDetailNotFoundException {
+    BookDetail bookDetailFromDb = getBookDetail(bookId);
+
+    bookDetailFromDb.setSummary(bookDetail.getSummary());
+    bookDetailFromDb.setPageCount(bookDetail.getPageCount());
+    bookDetailFromDb.setYear(bookDetail.getYear());
+    bookDetailFromDb.setIsbn(bookDetail.getIsbn());
+
+    return bookDetailRepository.save(bookDetailFromDb);
+  }
+
+  public BookDetail removeBookDetail(Long bookId)
+      throws BookNotFoundException, BookDetailNotFoundException {
+    Book book = findById(bookId);
+    BookDetail bookDetail = book.getDetail();
+
+    if (bookDetail == null) {
+      throw new BookDetailNotFoundException();
+    }
+
+    book.setDetail(null);
+    bookDetail.setBook(null);
+
+    bookDetailRepository.delete(bookDetail);
+
+    return bookDetail;
   }
 }
