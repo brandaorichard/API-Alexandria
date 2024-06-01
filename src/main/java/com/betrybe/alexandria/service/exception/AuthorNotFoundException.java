51c2f24a6f496897eaa48216d22b143140f0d0ef
brandaorichard <rih.brandao@gmail.com>
2024-06-01 01:43:16 -0300

    feat: additional changes to exceptions

--- src/main/java/com/betrybe/alexandria/service/exception/AuthorNotFoundException.java
+++ src/main/java/com/betrybe/alexandria/service/exception/AuthorNotFoundException.java
@@ -1,13 +1,7 @@
 package com.betrybe.alexandria.service.exception;
 
-/**
- * The type Author not found exception.
- */
 public class AuthorNotFoundException extends NotFoundException {
 
-  /**
-   * Instantiates a new Author not found exception.
-   */
   public AuthorNotFoundException() {
     super("Pessoa autora não encontrada!");
   }
--- src/main/java/com/betrybe/alexandria/service/exception/BookNotFoundException.java
+++ src/main/java/com/betrybe/alexandria/service/exception/BookNotFoundException.java
@@ -1,13 +1,7 @@
 package com.betrybe.alexandria.service.exception;
 
-/**
- * The type Book not found exception.
- */
 public class BookNotFoundException extends NotFoundException {
 
-  /**
-   * Instantiates a new Book not found exception.
-   */
   public BookNotFoundException() {
     super("Livro não encontrado!");
   }
