d041b8b391b4056105a14adf4908aa15458ab4b6
brandaorichard <rih.brandao@gmail.com>
2024-06-02 03:40:50 -0300

    add annotations to GlobalControllerAdvice

--- src/main/java/com/betrybe/alexandria/advice/GlobalControllerAdvice.java
+++ src/main/java/com/betrybe/alexandria/advice/GlobalControllerAdvice.java
@@ -4,8 +4,10 @@ import com.betrybe.alexandria.service.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
 public class GlobalControllerAdvice {
 
   @ExceptionHandler
